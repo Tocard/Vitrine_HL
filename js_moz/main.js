8f3e7114dcc3db47070d5074899fe9399812c485
FG5D627N <florian-externe.goudot@enedis.fr>
2019-01-16 09:38:25 +0100

    var to let

--- js_moz/main.js
+++ js_moz/main.js
@@ -1,8 +1,8 @@
-var array = ["slide 1", "slide 2", "slide 3", "slide 4", "slide 5"]
+let array = ["slide 1", "slide 2", "slide 3", "slide 4", "slide 5"];
 
 array.forEach(function(item) {
-    var li = document.createElement("li");
-    var text = document.createTextNode(item);
+    let li = document.createElement("li");
+    let text = document.createTextNode(item);
     li.appendChild(text);
     document.getElementById("myUl").appendChild(li);
 });
